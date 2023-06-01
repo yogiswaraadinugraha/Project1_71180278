402072b00618e8db930cab2042fd4a8fd0a9e544
yogiswaraadinugraha <45014502+yogiswaraadinugraha@users.noreply.github.com>
2023-06-01 16:29:01 +0700

    Tugas Akhir 71180278

---
+++ app/src/main/java/com/example/project1_71180278/KeduaHalaman.kt
@@ -0,0 +1,11 @@
+package com.example.project1_71180278
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KeduaHalaman:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.halaman_kedua)
+    }
+}
---
+++ app/src/main/java/com/example/project1_71180278/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.project1_71180278
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val i = Intent (this, KeduaHalaman :: class.java)
+        startActivity(i)
+    }
+}
