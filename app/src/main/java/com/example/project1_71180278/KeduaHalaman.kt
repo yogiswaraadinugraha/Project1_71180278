0ae7eadfc901238bbd07fd47797933f473734047
yogiswaraadinugraha <45014502+yogiswaraadinugraha@users.noreply.github.com>
2023-06-01 16:55:24 +0700

    Project_71180278

--- app/src/main/java/com/example/project1_71180278/KeduaHalaman.kt
+++ app/src/main/java/com/example/project1_71180278/KeduaHalaman.kt
@@ -2,10 +2,18 @@ package com.example.project1_71180278
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.TextView
 
 class KeduaHalaman:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.halaman_kedua)
+
+        val txVnmaBarang2 = findViewById<TextView>(R.id.textView17)
+
+        val nama = txVnmaBarang2.text
+
+
     }
 }
--- app/src/main/java/com/example/project1_71180278/MainActivity.kt
+++ app/src/main/java/com/example/project1_71180278/MainActivity.kt
@@ -7,11 +7,22 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txVnmaBarang = findViewById<TextView>(R.id.textView)
+        val nmaBarang = txVnmaBarang.text
+
+        val txVhrgaBarang = findViewById<TextView>(R.id.textView4)
+        val hrgaBrng = txVhrgaBarang.text
+
+        val btnBeli = findViewById<Button>(R.id.button3)
 
         val i = Intent (this, KeduaHalaman :: class.java)
         startActivity(i)
